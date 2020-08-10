8fd33f5c89cf6d58324ada9f2dcdd939f1d23b8c
Felipe Pineda <fpineda@live.unc.edu>
2020-08-10 15:06:04 -0400

    set up socket.io and made sure it could recognize clients connecting to it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
-
+// configure web server and socket
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 const port = process.env.PORT || 3000 
+
 
 // Define paths for express configuration
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -12,6 +17,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 
-app.listen(port, () => {
+// event for when socket gets new connection
+io.on('connect', () =>{
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log('Server has successfully started on port: '+ port)
 })
