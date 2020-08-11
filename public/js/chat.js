5edc6fc32073da2176e0f7af89340e28ecca8cf2
Felipe Pineda <fpineda@live.unc.edu>
2020-08-11 16:04:08 -0400

    Client broadcast messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,14 @@
 const socket = io()
 
-// Client listening for an event from the server
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
+// Client receiving message event
+socket.on('message', (welcomeMessage) =>{
+    console.log(welcomeMessage)
 })
 
-document.querySelector('#increment').addEventListener('click', () =>{
-    console.log('click')
-    
-    // Client emitting an event for the server to listen
-    // every time a client clicks it emits an event
-    socket.emit('increment')
+const broadcastForm = document.querySelector('#message-form')
+broadcastForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const broadcastMessage = e.target.elements.message.value
+    // Client emits broadcast message event
+    socket.emit('sendMessage', broadcastMessage)
 })
--- src/index.js
+++ src/index.js
@@ -16,23 +16,20 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Setup static directory to serve (html, css, images, js)
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
 
 // Server listening for an event from the client (auto done)
 // event for when socket gets new connection
 io.on('connect', (socket) =>{
     console.log('New WebSocket connection')
 
-    // Server emitting an event for the Client to listen
-    socket.emit('countUpdated', count)
+    // Server emitting message event
+    let welcomeMessage = "Welcome!"
+    socket.emit('message', welcomeMessage)
 
-    // Server listening for an event from the client
-    socket.on('increment', () => {
-        count++
-        // emits to that single connection
-        // socket.emit('countUpdated', count)
-        // emits to all connection
-        io.emit('countUpdated', count)
+    // Server receiving broadcast message event
+    socket.on('sendMessage', (broadcastMessage) => {
+        // Server emitting broadcast message event as message event
+        io.emit('message', broadcastMessage)
     })
 })
 
