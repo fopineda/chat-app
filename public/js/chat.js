470b37c836700c928fa62b6e2eb5f72db739796c
Felipe Pineda <fpineda@live.unc.edu>
2020-08-12 16:30:36 -0400

    Set up front end buttons and input

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,27 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+
+
+
 // Client receiving message event
 socket.on('message', (welcomeMessage) =>{
     console.log(welcomeMessage)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
     const broadcastMessage = e.target.elements.message.value
     // Client emits broadcast message event
     socket.emit('sendMessage', broadcastMessage, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
         if (error){
             return console.log(error)
         }
@@ -21,6 +33,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
     if (!navigator.geolocation){
         return alert('Geolocation is not supported by your browser...')
     }
+    $locationButton.setAttribute('disabled', 'disabled')
     // Client emitting location event
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation',  {
@@ -28,6 +41,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
             longitude: position.coords.longitude
         }, () => {
             console.log('Location shared!')
+            $locationButton.removeAttribute('disabled')
         })
     })
 })
